563160e6ebf6e1decc1f79ee3ee3f9db6ca0dd78
oliullahakib <oliullahakib@gmail.com>
2026-05-03 20:05:57 +0600

    configaration for data handaling and others

---
+++ Readme.md
@@ -0,0 +1,1 @@
+This is a youtub type video handaling api backend
--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import dotenv from "dotenv"
 import connectToDB from "./db/index.js"
+import { app } from "./app.js"
 dotenv.config({path:'./.env'})
+const port = process.env.PORT || 8000
 connectToDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log('Error',error)
+        throw error
+    })
+    app.listen(port,()=>{
+        console.log(`Server is running on port ${port}`);       
+    })
+})
+.catch((err)=>{
+    console.log("Mongodb connection faild",err);
+    
+})
